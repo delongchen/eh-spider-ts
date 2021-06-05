97af8e62a25636f6d52d62ff6143add3d25b8370
delongchen <fg456@vip.qq.com>
2021-06-06 01:28:34 +0800

    add ehs buffer

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,9 +1,19 @@
 import { router } from '../service/koa'
 import { getAllList } from "../data/redis";
 
+
+let itemsCache: Buffer = null
+let updateTime: number = Date.now() + 3600 * 1000
+
 router.get('/ehs', async (context) => {
-  const items = await getAllList('eh', 0, -1) as string[]
+  const reqTime = Date.now()
+  if (itemsCache === null || updateTime < reqTime) {
+    const items = await getAllList('eh', 0, -1) as string[]
+    itemsCache = new Buffer(`[${items.join(',')}]`)
+    updateTime =  reqTime + 3600 * 1000
+  }
   context.set('Content-Type', 'application/json')
   context.set('Access-Control-Allow-Origin', '*')
-  context.body = `[${items.join(',')}]`
+  context.set('Cache-Control', 'no-cache, max-age=0')
+  context.body = itemsCache
 })
